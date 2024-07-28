9f33f69cc2b944f2efb595bcd7097f42dfded9e8
cw3st <156005055+cw3st@users.noreply.github.com>
2024-07-28 15:15:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    allowTouchMove:false,
+    grabCursor: false,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    mousewheel:true
+});
+swiper.sliderMove = function (s, e) {
+    console.log(s)
+}
+function Navigate(indx) {
+    for (let i of document.querySelectorAll(".Links li")) i.classList.remove("activeLink")
+    Array.from(document.querySelectorAll(".Links li"))[indx].classList.add("activeLink")
+    swiper.slideTo(indx, 1000, true)
+}
+
+function play(){
+    let xy = document.getElementById("rlist")
+    xy.style.display = "block"
+    let cc = document.getElementById("srnav")
+    cc.style.display = "block"
+    let br = document.getElementById("rnav")
+    br.style.display = "none"
+}
+
+function cod(){
+    let xy = document.getElementById("rlist")
+    xy.style.display = "none"
+    let br = document.getElementById("srnav")
+    br.style.display = "none"
+    let hh = document.getElementById("rnav")
+    hh.style.display ="block"
+}
